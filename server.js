0c36503fde7cf67f7cb9edc210e513d4ad3077c5
Kaian Santos <kaianfsantos@gmail.com>
2023-05-09 23:15:12 -0300

    feat: adding chatGPT to the bot

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import { Client, GatewayIntentBits } from 'discord.js'
-
+import axios from 'axios'
 import {
   joinVoiceChannel,
   VoiceConnectionStatus,
   createAudioPlayer,
-  createAudioResource
+  createAudioResource,
 } from '@discordjs/voice'
 
 
@@ -28,7 +28,10 @@ const commands = {
 
       const channelId = interaction.member.voice.channelId
       const guildId = interaction.member.guild.id
-      const stream = ytdl(musicURL, { filter: 'audioonly' })
+      const stream = ytdl(musicURL, {
+        filter: 'audioonly',
+        highWaterMark: 1 << 62,
+      })
       stream.on('info', async info => {
         await interaction.reply(`Playing: ${info.videoDetails.title }`)
       })
@@ -41,12 +44,21 @@ const commands = {
       connection.subscribe(player)
       player.play(resource)
       connection.on(VoiceConnectionStatus.Ready, () => {
-        console.log('here')
       })
     } catch (error) {
       console.log(error)
     }
 
+  },
+  gpt: async (interaction) => {
+    interaction.reply('Estou pensando...')
+    const channel = client.channels.cache.get(interaction.channelId)
+
+    const question = interaction.options.data[0].value
+    const { data } = await axios.post('http://localhost:3000/chat', {
+      message: question
+    })
+    await channel.send(`pergunta: ${question} \n resposta: ${data}`)
   },
   pause: () => {
 
@@ -66,7 +78,7 @@ client.on('messageCreate', msg => {
   if (msg.content === '!play') {
     const voiceChannel = msg.channelId
     voiceChannel.join().then(connection => {
-      const stream = ytdl('https://www.youtube.com/watch?v=I4YDuG4XYtw&ab_channel=ternorei', { filter: 'audioonly' })
+      const stream = ytdl('https://www.youtube.com/watch?v=I4YDuG4XYtw', { filter: 'audioonly' })
       const dispatcher = connection.play(stream)
       dispatcher.on('end', () => voiceChannel.leave())
     })
